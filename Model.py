6cad76fb3b8d2b1265ac5a2cc1d00922c02ab488
ProKobSha <163946616+ProKobSha@users.noreply.github.com>
2025-01-27 20:44:19 -0600

    Model.py

---
+++ Model.py
@@ -0,0 +1,21 @@
+from app import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(10), default='standard')  # 'moderator' o 'standard'
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    uploaded_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
